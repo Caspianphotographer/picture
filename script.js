7830273b0a5f5e4274c4dbd4e6858d265082f094
Caspianphotographer <amirphotomahdi@gmail.com>
2025-10-25 14:12:20 +0330

    Update script.js

--- script.js
+++ script.js
@@ -100,3 +100,14 @@ prevBtn.addEventListener("click", showPrev);
 lightbox.addEventListener("click", e => {
   if (e.target === lightbox) closeLightbox();
 });
+
+
+// منوی موبایل
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if(menuToggle){
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
